eb0a8b675ca0c6f2b0fb8a7157e025b59aca1382
pGumowski38 <p.gumowski38@gmail.com>
2021-02-10 20:14:10 +0100

    Add Files

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login_page():
+    return render_template('login_page.html')
+
+
+@app.route('/register')
+def register_page():
+    return render_template('register_page.html')
+
+
 if __name__ == '__main__':
     app.run()
